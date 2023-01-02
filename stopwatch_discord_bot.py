1544f0a155a5513dadd722f57e93c199c4821c72
sirpellinore <121779386+sirpellinore@users.noreply.github.com>
2023-01-01 20:15:57 -0700

    Update stopwatch_discord_bot.py

--- stopwatch_discord_bot.py
+++ stopwatch_discord_bot.py
@@ -8,16 +8,6 @@ def time_convert(sec):
     str = "{0}:{1}:{2}".format(int(hours),int(mins),sec)
     return str
 
-"""function that never got used
-accumulated_time=0
-
-def stopwatch(start_time):
-    end_time = time.time()
-    time_lapsed = end_time - start_time
-    accumulated_time += time_lapsed
-    time_info = ["Time elapsed:",time_convert(time_lapsed),"Accumulated time:",time_convert(accumulated_time)]
-    return time_info """
-
 def convert_list_into_string(list):
     string = ""
     for element in list:
@@ -35,43 +25,11 @@ def print_after_space(str):
     str = str[index+1:]
     return str
 
-"""function that never got used
-def has_space(str):
-    for i in range(len(str)):
-        if str[i] == ' ':
-            bool = True
-            break
-        else:
-            bool = False
-    return bool"""
-
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-
-
-""" make stopwatch bot speak """
-
-"""
-@client.event
-async def on_ready():
-    channel_info = client.get_channel(1054253491238993940)
-    await channel_info.send("Hello. I am the only bot on the server")
-    await channel_info.send("Using me is easy! There are 3 commands!")
-    await channel_info.send("The commands are 1) !activateStopwatch")
-    await channel_info.send("2) !deactivateStopwatch and 3) !calculateTime")
-    await channel_info.send("C1 does exactly what it says! It activates the stopwatch!")
-    await channel_info.send("C2 deactivates the stopwatch!")
-    await channel_info.send("C3 is a little tricky. You have to type...")
-    await channel_info.send("!calculateTime followed a space, then the number that the stopwatch sent when you activated it, then another space, and finally the number the stopwatch sent when you deactivated it")
-    await channel_info.send("Like this:")
-    await channel_info.send("!calculateTime 123 345")
-    await channel_info.send("That is honestly all there is to using the stopwatch bot! Have fun with it!!")"""
-
-""" make stopwatch bot respond to commands"""
 
 
 @client.event
@@ -97,4 +55,4 @@ async def on_message(message):
     
 
 
-client.run('MTA1NDExODgzNjEyNDc3ODUyNg.Gs9MTZ.k04iEY3gM8P9E9GkO_mXtgi6nVAKeGGCirNAhY')
+client.run(token)
